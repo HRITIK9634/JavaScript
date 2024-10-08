6a90388887d02778ba0e50653d814a2cc02156ef
HRITIK9634 <sainihritik087@gmail.com>
2024-10-08 16:03:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+//Convert Number to String
+let score = 33;
+console.log(typeof score);
+
+let string = String (score);
+console.log(typeof string);
+console.log(string);
+
+//type string
+//value 33
+
+//Convert String to Number
+let name = "hritik";
+
+let num = Number (name);
+console.log(typeof num);
+console.log(num);
+
+//type number
+//value NaN
+
+
+
+//"33" => 33       (type num, value 33)
+//"3abc" => Nan    (type num, value Nan)
+//true => 1        (type num, value 1)
+//false => 0       (type num, value 0)
+//"hritik" => true     (type boolean, value true )
