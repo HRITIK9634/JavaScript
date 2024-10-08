233fde6b50bbae1ab5391b1ed819fbf18e6076e3
HRITIK9634 <sainihritik087@gmail.com>
2024-10-08 15:30:43 +0000

    ddatatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+let rollNum = 12;
+let name = "hritik";
+let isPass = true;
+let num = null;
+let age;
+
+console.log(typeof rollNum);
+console.log(typeof name);
+console.log(typeof isPass);
+console.log(typeof num);
+console.log(num);
+console.log(typeof age);
+
+
+//num 
+//bigint
+//string => ""
+//boolean => true/false
+//null => standslone value
+//undefine
+//symbol => unique
+//object
