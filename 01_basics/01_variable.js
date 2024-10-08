5599ea612599ad321eea5dca6778df5e2af12b91
HRITIK9634 <sainihritik087@gmail.com>
2024-10-08 15:16:45 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 123456;
+let accountEmail =  "hritk@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+
+console.log([accountId, accountEmail,  accountPassword, accountCity]);
+
+console.table([accountId, accountEmail,  accountPassword, accountCity]);
+
+
+//const value can't be change and reassigne 
+//let value can be change but can't be reassigne
+//const value can be change and reassigne
+
