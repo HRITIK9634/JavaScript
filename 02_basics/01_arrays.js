4f695f77d40e553a718cb3958ca7c272bea0b494
HRITIK9634 <sainihritik087@gmail.com>
2024-10-09 15:47:58 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,42 @@
+// Array
+
+// const myArr = [0,1,2,43,4,5];
+
+// const myArr2 = new Array(1,2,3,4);
+// console.log(myArr[3]);
+
+
+// // Array Methos
+
+// myArr.push(6);  // add element end
+// console.log(myArr);
+
+// myArr.pop();   // remove element end
+// console.log(myArr);
+
+// myArr.unshift(9);   // add element start
+// console.log(myArr);
+
+// myArr.shift();   // remove element start
+// console.log(myArr);
+
+// console.log(myArr.includes(3));
+// console.log(myArr.includes(2));
+// console.log(myArr.indexOf(2));
+
+
+// const newArr = myArr.join();  // it's change the type of value array to string
+// console.log(newArr);
+// console.log(myArr);
+
+
+// slice , splice
+
+//it's create copy of arrya
+const arr = [1,2,3,4,5];
+console.log(arr.slice(1,3));
+console.log(arr);
+
+//it's change original array
+console.log(arr.splice(1,3));
+console.log(arr);
