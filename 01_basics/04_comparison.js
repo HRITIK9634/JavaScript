0f9bc31a7d16dc278d28d00fb4e09a53cacda257
HRITIK9634 <sainihritik087@gmail.com>
2024-10-09 02:42:02 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// a+b
+// a-b
+// a*b
+// a/b
+// a**b
+// a%b
+
+
+//Avoid this type of comparison
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+
+// ===  strict check
+console.log("2" === 2); //it check type of value
