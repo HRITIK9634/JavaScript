b5027ae290a1477301a681064dc6da6e6cc9568a
HRITIK9634 <sainihritik087@gmail.com>
2024-10-09 02:25:18 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -6,12 +6,11 @@ let string = String (score);
 console.log(typeof string);
 console.log(string);
 
-//type string
-//value 33
+// //type string
+// //value 33
 
 //Convert String to Number
 let name = "hritik";
-
 let num = Number (name);
 console.log(typeof num);
 console.log(num);
@@ -26,3 +25,23 @@ console.log(num);
 //true => 1        (type num, value 1)
 //false => 0       (type num, value 0)
 //"hritik" => true     (type boolean, value true )
+
+
+
+
+//  *********************Operation**********************/
+
+
+let str1 = "hritik";
+let str2 = " saini";
+let str3 = str1 + str2;
+console.log(str3);
+
+
+// console.log(("1" + 2));
+// console.log((1 + "2"));
+// console.log(("1" + 2 + 2));
+// console.log((1 + 2 + "2"));
+
+
+
