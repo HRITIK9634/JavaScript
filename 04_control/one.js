c8f3e695088397236b36c3f81947e5df8d92cfea
HRITIK9634 <sainihritik087@gmail.com>
2024-10-11 15:04:21 +0000

    condition

---
+++ 04_control/one.js
@@ -0,0 +1,37 @@
+// if
+
+// const isUserLoggedIn = true;
+// if(isUserLoggedIn){
+//     console.log("executed");
+// }
+
+
+//  if else
+
+// const balance = 1000;
+// if(balance > 500){
+//     console.log("balance is greater");
+// }
+// else{
+//     console.log("balance is lesser");
+// }
+
+
+// Nested
+// const balance = 1000;
+
+// if(balance < 500){
+//     console.log("less than 500");
+// }
+// else if(balance < 750){
+//     console.log("less than 750")
+// }
+
+// else if(balance < 900){
+//     console.log("less than 750")
+// }
+// else{
+//     console.log("less than 1200 = ",balance);
+// }
+
+
