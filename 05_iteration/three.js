3405e597df63def93dd46adcafe93e6e2cf33d0a
HRITIK9634 <sainihritik087@gmail.com>
2024-10-11 16:52:20 +0000

    forofloop

---
+++ 05_iteration/three.js
@@ -0,0 +1,38 @@
+// for of loop
+
+// let myArr = [1,2,3,4,5];
+// for (const num of myArr) {
+//     console.log(num);
+// }
+
+
+// let str = "hritik";
+// for (const num of str) {
+//     console.log(num);
+// }
+
+
+
+// Maps
+
+// const map = new Map();
+// map.set('IN', "India");
+// map.set('USA', "United Stateof America");
+
+
+// for(const [key] of map){
+//     console.log(key);
+// }
+
+
+
+// const obj ={
+//     'game1': 'Nfs',
+//     'game2': 'spiderman'
+// }
+
+// for(const [key, value] of obj){
+//     console.log(key, ':-', value);
+// }
+
+
