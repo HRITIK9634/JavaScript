e8a05be90ccb6d9d4640d48b11f454eb25db896b
HRITIK9634 <sainihritik087@gmail.com>
2024-10-11 15:51:57 +0000

    forloop

---
+++ 05_iteration/one.js
@@ -0,0 +1,46 @@
+// for
+
+// for(let i=1; i<=10; i++){
+//     const element = i;
+//     if(element == 5 ){
+//         console.log("5 is the best number");
+//     }
+//     console.log(element);
+// }
+
+
+
+// for(let i=1; i<=10; i++){
+//     for(let j=1; j<=10; j++){
+//         console.log(i + '*' + j + '=' + i*j);
+//     }
+//     console.log();
+// }
+
+
+
+// let myArr = ["ironman","superman","thor"];
+// for(let index=0; index<myArr.length; index++){
+//     const element = myArr[index];
+//     console.log(element);
+// }
+
+
+// break and continue
+
+// for(let i=1; i<=20; i++){
+//     if (i == 5){
+//         console.log("value is five");
+//         break;
+//     }
+//     console.log(`value of i is ${i}`);
+// }
+
+
+for(let i=1; i<=20; i++){
+        if (i == 5){
+            console.log("value is five");
+            continue;
+        }
+        console.log(`value of i is ${i}`);
+    }
