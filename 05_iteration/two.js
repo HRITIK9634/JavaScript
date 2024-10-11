e189db63558067bc294acd56cf45aaf5509d14ad
HRITIK9634 <sainihritik087@gmail.com>
2024-10-11 16:03:56 +0000

    whileanddowhile

---
+++ 05_iteration/two.js
@@ -0,0 +1,24 @@
+// while loop
+
+// let i = 1;
+// while(i <= 10 ){
+//     console.log(`value of i is ${i}`);
+//     i += 2;
+// }
+
+
+// let myArr = ["hritik","rajat","anand"];
+// let arr = 0;
+// while(arr < myArr.length){
+//     console.log(`value of ${myArr[arr]}`);
+//     arr++;
+// }
+
+
+let score = 1;
+
+
+do{
+    console.log(`score is ${score}`);
+    score++
+}while(score <= 10);
