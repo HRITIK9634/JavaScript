fcf6141ffdf99de606df0d719e3827d3bc78b3cb
HRITIK9634 <sainihritik087@gmail.com>
2024-10-12 15:11:41 +0000

    fillter

---
+++ 05_iteration/six.js
@@ -0,0 +1,30 @@
+
+// filter
+
+//acces value by filter method
+// const num = [1,2,3,4,5,6];
+
+// const myNum = num.filter( (ele) => {
+//     return ele > 3;
+// });
+
+
+
+
+
+//acces value by forEach loop
+
+// const newNums = [];
+
+// num.forEach((element) => {
+//     if(element > 4){
+//         newNums.push(element);
+//     }
+// });
+
+// console.log(newNums);
+
+
+
+
+
