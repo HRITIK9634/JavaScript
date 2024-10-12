e193995d508266830c6bcc89ed8a4124c24b8103
HRITIK9634 <sainihritik087@gmail.com>
2024-10-12 14:30:48 +0000

    forInloop

---
+++ 05_iteration/four.js
@@ -0,0 +1,30 @@
+// for in loop
+
+// const myObj = {
+
+//     js: 'javascript',
+//     cpp: 'c++',
+//     rb: 'ruby',
+//     swift: 'swift by apple'
+// }
+
+// for (const key in myObj) {
+//     console.log(`${key} shorcut is for ${myObj[key]}`);
+// }
+
+
+// const programming = ['js','cpp','java','python'];
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+
+
+// const map = new Map();
+// map.set('IN', "India");
+// map.set('USA', "United Stateof America");
+
+
+// for (const key in map) {
+//     console.log(key)
+// }
